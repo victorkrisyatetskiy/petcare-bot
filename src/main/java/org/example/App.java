2f5f1117fa3616d3d3b2d0e91b9a30b76d5793b5
Victor Krisyatetskiy <victorkrisyatetskiy@gmail.com>
2025-09-17 15:05:52 +0200

    Initial commit: project setup with Telegram bot dependency

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new PetCareBot());
+            System.out.println("Bot is ready for work");
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/org/example/PetCareBot.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class PetCareBot extends TelegramLongPollingBot {
+        @Override
+        public String getBotToken(){
+            return "";
+        }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+
+            SendMessage message = new SendMessage();
+            message.setChatId(String.valueOf(chatId));
+
+            if (messageText.equals("/start")){
+                message.setText("Hello! I am your bot for pet care!");
+            }else {
+                message.setText("Sorry, I don't understand the request");
+            }try{
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "";
+    }
+}
